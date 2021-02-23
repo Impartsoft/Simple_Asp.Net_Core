a524a908a9b51f456022598dcd893d7ae7ce235f
15280231921 <a210129917>
2021-02-23 22:16:15 +0800

    CROS TEST

--- Simple_Asp.Net_Core 2.CORS/Simple_Asp.Net_Core/ServiceProvider/CORS.cs
+++ Simple_Asp.Net_Core 2.CORS/Simple_Asp.Net_Core/ServiceProvider/CORS.cs
@@ -6,15 +6,16 @@ namespace Simple_Asp.Net_Core.ServiceProvider
     {
         public static void AddCORS(this IServiceCollection services)
         {
-            //services.AddCors(
-            //   options => options.AddPolicy(
-            //       "CorsTest",
-            //        // 配置前端域名，注意端口号后不要带/斜杆
-            //        p => p.AllowAnyOrigin()
-            //             //p => p.WithOrigins("https://localhost:44372", "https://localhost:44372")
-            //             .AllowAnyHeader()
-            //             .AllowAnyMethod()
-            //             .WithExposedHeaders("Content-Disposition")));
+                services.AddCors(
+                   options => options.AddPolicy(
+                       "CorsTest",
+                        // 目前先运行所有请求，当得知前端域名的时候，再改成WithOrigins方式
+                        p => p.AllowAnyOrigin()
+                             // 配置前端域名，注意端口号后不要带/斜杆
+                             //p => p.WithOrigins("https://localhost:44372", "https://localhost:44372")
+                             .AllowAnyHeader()
+                             .AllowAnyMethod()
+                             .WithExposedHeaders("Content-Disposition")));
         }
     }
 }
