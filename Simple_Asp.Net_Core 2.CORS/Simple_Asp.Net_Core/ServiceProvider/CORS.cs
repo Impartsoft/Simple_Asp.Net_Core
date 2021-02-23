bd1aa2d7761026bdb5ecf01eede9948b27ee04bb
15280231921 <a210129917>
2021-02-23 22:17:08 +0800

    CORS

--- Simple_Asp.Net_Core 2.CORS/Simple_Asp.Net_Core/ServiceProvider/CORS.cs
+++ Simple_Asp.Net_Core 2.CORS/Simple_Asp.Net_Core/ServiceProvider/CORS.cs
@@ -9,7 +9,7 @@ namespace Simple_Asp.Net_Core.ServiceProvider
                 services.AddCors(
                    options => options.AddPolicy(
                        "CorsTest",
-                        // 目前先运行所有请求，当得知前端域名的时候，再改成WithOrigins方式
+                        // 目前先允许所有请求，当能够明确前端域名的时候，再改成WithOrigins方式
                         p => p.AllowAnyOrigin()
                              // 配置前端域名，注意端口号后不要带/斜杆
                              //p => p.WithOrigins("https://localhost:44372", "https://localhost:44372")
