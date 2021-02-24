b5ea823aa660864dfa154e8c76b35f26bee8afc0
T_T <46785596+IanROX@users.noreply.github.com>
2021-02-24 13:37:08 +0800

    增加警告处理

---
+++ Simple_Asp.Net_Core 3.AutoMapper Restful/Simple_Asp.Net_Core/Profiles/Class.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Simple_Asp.Net_Core.Dtos;
+using Simple_Asp.Net_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_Asp.Net_Core.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //Source -> Target
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
