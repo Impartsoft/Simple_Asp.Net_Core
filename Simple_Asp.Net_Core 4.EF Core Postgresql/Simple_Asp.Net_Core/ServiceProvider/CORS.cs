0a7fbcefae7a272188ee7b5f9d23a7550a6a75f9
T_T <46785596+IanROX@users.noreply.github.com>
2021-02-24 17:21:17 +0800

    EF  Core

---
+++ Simple_Asp.Net_Core 4.EF Core Postgresql/Simple_Asp.Net_Core/ServiceProvider/CORS.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Simple_Asp.Net_Core.ServiceProvider
+{
+    public static class CORS
+    {
+        public static void AddCORS(this IServiceCollection services)
+        {
+                services.AddCors(
+                   options => options.AddPolicy(
+                       "CorsTest",
+                        // 目前先允许所有请求，当能够明确前端域名的时候，再改成WithOrigins方式
+                        p => p.AllowAnyOrigin()
+                             // 配置前端域名，注意端口号后不要带/斜杆
+                             //p => p.WithOrigins("https://localhost:44372", "https://localhost:44372")
+                             .AllowAnyHeader()
+                             .AllowAnyMethod()
+                             .WithExposedHeaders("Content-Disposition")));
+        }
+    }
+}
