32a31c962534d761067e8e254fc620e60d65bd7e
T_T <46785596+IanROX@users.noreply.github.com>
2021-02-25 15:59:24 +0800

    sdk update & jwt

---
+++ Simple_Asp.Net_Core 5.SDK Update & JWT/Simple_Asp.Net_Core/ServiceProvider/JWT.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Asp.Net_Core.ServiceProvider
+{
+    public static class JWT
+    {
+        public static void AddJWT(this IServiceCollection services)
+        {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddJwtBearer(o =>
+           {
+               o.RequireHttpsMetadata = false;
+               o.Events = new JwtBearerEvents()
+               {
+                   OnAuthenticationFailed = context =>
+                   {
+                       //Token expired
+                       if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                           context.Response.Headers.Add("Token-Expired", "true");
+
+                       return Task.CompletedTask;
+                   },
+               };
+
+               o.TokenValidationParameters = new TokenValidationParameters
+               {
+                   // 是否验证失效时间
+                   ValidateLifetime = true,
+                   ClockSkew = TimeSpan.FromSeconds(30),
+                   ValidateAudience = true,
+                   // 这里采用动态验证的方式，在重新登陆时，刷新token，旧token就强制失效了
+                   AudienceValidator = AudienceValidator,
+                   // 是否验证Issuer
+                   ValidateIssuer = false,
+                   // 是否验证SecurityKey
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Const.SecurityKey))
+               };
+           });
+        }
+
+        private static bool AudienceValidator(IEnumerable<string> audiences, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            return Audiences.IsNewestAudience(audiences.FirstOrDefault());
+        }
+    }
+}
---
+++ Simple_Asp.Net_Core 5.SDK Update & JWT/Simple_Asp.Net_Core/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+using Simple_Asp.Net_Core.Data;
+using Simple_Asp.Net_Core.ServiceProvider;
+using System;
+
+namespace Simple_Asp.Net_Core
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddJWT();
+
+            services.AddDbContext<CommanderContext>(options =>
+                options.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=123456"));
+
+            services.AddCORS();
+            services.AddMvc();
+            services.AddSwagger();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
+
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                });
+            }
+            app.UseCors("CorsTest");
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+        }
+    }
+}
