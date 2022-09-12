21728253e79d291db4d9e43789cb590bae0c319e
Harry <364424187@qq.com>
2022-09-12 21:58:42 +0800

    增加用户图片与简介字段
    增加用户图片与简介字段

--- Simple_Asp.Net_Core/Simple_Asp.Net_Core/Migrations/20220912135723_UserPhotoAndResumeColumns.Designer.cs
+++ Simple_Asp.Net_Core/Simple_Asp.Net_Core/Migrations/20220912135723_UserPhotoAndResumeColumns.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Simple_Asp.Net_Core.Model.DBContext;
@@ -11,9 +12,10 @@ using Simple_Asp.Net_Core.Model.DBContext;
 namespace Simple_Asp.Net_Core.Migrations
 {
     [DbContext(typeof(CommanderContext))]
-    partial class CommanderContextModelSnapshot : ModelSnapshot
+    [Migration("20220912135723_UserPhotoAndResumeColumns")]
+    partial class UserPhotoAndResumeColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Simple_Asp.Net_Core/Simple_Asp.Net_Core/Migrations/20220912135723_UserPhotoAndResumeColumns.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simple_Asp.Net_Core.Migrations
+{
+    public partial class UserPhotoAndResumeColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "tbUser",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resume",
+                table: "tbUser",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "tbUser");
+
+            migrationBuilder.DropColumn(
+                name: "Resume",
+                table: "tbUser");
+        }
+    }
+}
