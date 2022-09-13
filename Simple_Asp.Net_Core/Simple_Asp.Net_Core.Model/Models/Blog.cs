4127baab4860e0735e7e8da2e9e55d235b3eedf5
Harry <364424187@qq.com>
2022-09-13 22:35:17 +0800

    add blog and comment
    add blog and comment

---
+++ Simple_Asp.Net_Core/Simple_Asp.Net_Core.Model/Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using Simple_Asp.Net_Core.Models;
+
+namespace Simple_Asp.Net_Core.Model.Models
+{
+    public class Blog : EntityBase
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public User User { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public IList<Comment> Comments { get; set; }
+    }
+}
---
+++ Simple_Asp.Net_Core/Simple_Asp.Net_Core.Model/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿namespace Simple_Asp.Net_Core.Model.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+
+        public User User { get; set; }
+
+        public string UserId { get; set; }
+
+        public Blog Blog { get; set; }
+
+        public Guid BlogId { get; set; }
+    }
+}
